fa4f6e6b7ddf8ef92f1ec55633d42d04e5c788a9
M-Riosa <mriosa7@gmail.com>
2023-07-08 17:02:00 -0400

    Finished site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,120 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Main } from 'next/document'
+import  Head  from "next/head";
+import {BsFillMoonStarsFill} from 'react-icons/bs';
+import {AiFilTwitterCircle, AiFillLinkedin, AiFillGithub} from "react-icons/ai";
+import { useState } from "react";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className =  {darkMode ? "dark" : ""}>
+    <Head>
+      <title> Matias Riosa Portfolio</title>
+      <meta name="description" content="Generateed by me" />
+      <link rel ="icon" href="/favicon.ico"/>
+    </Head>
+    <main className='bg-white px-10 md:text-center md:px-20 lg:px-40 dark:bg-gray-950 dark:text-white'>
+       <section  className='min-h-screen'>
+         <nav className='py-10 mb-12 flex justify-between'>
+          <h1 className='text-xl font-serif'>Matias Riosa's Portfolio</h1>
+          <ul className = "flex items-center">
+            <li><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl'/></li>
+          </ul>
+         </nav>
+         <div className='text-center pt-10 pb-3'>
+            <h2 className='text-5xl py-2 font-medium md:text-4xl'>Matias Riosa</h2>
+            <h3 className='text-2xl py-2'>Computer Science student at UCF</h3>
+            <p className='text-md pt-5 pb-1 leading-8 text-gray-500 dark:text-gray-200'>About me: </p>
+            <p class='text-md pt-1 pb-1 leading-8 text-gray-500 max-w-3xl mx-auto  dark:text-gray-200'>
+                Hello, I am a current student at the University of Central Florida studying Computer Science.
+                I have completed degrees in Finance and Marketing in the past and after some time in the field I decided that 
+                wanted to pursue my interest in Computer Science, so after a great deal of individual learning I decided it would be most beneficial
+                to return to school. I am now currently on track to graduate in 2024.
+
+            </p> 
+            <p class='text-md pt-1 pb-1 leading-8 text-gray-500 max-w-3xl mx-auto  dark:text-gray-200'>
+                Outside of my professional life, I love spending time outside, travelling, and being born into an Argentine family has caused me to love
+                anything that has to do with futbol. Ideally in the future I would love the opportunity to work in a position that constantly challenges me 
+                and causes me to learn new things. I would also love the opportunity to work in different countries around the world.
+            </p>
+         </div>
+         <div className='text-5xl flex justify-center gap-16 py-3 text-gray-800 dark:text-gray-200'>
+            <button>
+             <a href="www.linkedin.com/in/matias-riosa-5a8921180" class="services__links">
+              <AiFillLinkedin/>
+              </a>
+            </button>
+            
+            <button>
+             <a href="https://github.com/m-riosa" class="services__links">
+              <AiFillGithub/>
+              </a>
+            </button>
+            
+         </div>
+         <div className='justify-center flex pt-5'>
+            <Image src="/images/linkedin.jpeg" width="250" height = "250"/>
+         </div>
+       </section>
+       <section>
+        <div>
+          <h3 className='text-3xl py-1 flex justify-center'>Projects I've worked on</h3>  
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <div className='gap-10 justify-center flex flex-col lg:flex-row lg:flex-wrap dark:text-gray-100'>
+          <div className='text-center shadow-lg p-10 my-10 basis-1/3 flex-1 dark:bg-gray-900'>
+              <div className='flex justify-center'>
+                <Image src="/images/Messi WC Picture.jpeg" width="250" height = "250"/>
+              </div>
+              <h3 className='text-lg font-medium'>FutHub app</h3>
+              <p className='text-gray-700 py-1 dark:text-white'>Tools used:</p>
+              <p className='text-gray-700 dark:text-white'>Java</p>
+              <p className='text-gray-700 dark:text-white'>Android Studio</p>
+              <p className='text-gray-700 dark:text-white'>Retrofit HTTP client</p>
+              <p className='text-gray-700 dark:text-white'>Postman API platform</p>
+              <div className= 'pt-5'>
+                <button class="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                  <a href="https://github.com/m-riosa/FutHub-app" class="services__links">
+                    View on GitHub
+                  </a>
+                </button>
+              </div>
+              
+          </div>
+          <div className='text-center shadow-lg p-10 my-10 basis-1/3 flex-1 dark:bg-gray-900'>
+              <div className='flex justify-center'>
+                <Image src="/images/othello.png" width="250" height = "250"/>
+              </div>
+              <h3 className='text-lg font-medium'>Othello Board game</h3>
+              <p className='text-gray-700 py-1 dark:text-white'>Tools used:</p>
+              <p className='text-gray-700 dark:text-white'>C</p>
+              <div className= 'pt-5'>
+                <button class="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                  <a href="https://github.com/m-riosa/othello-game" class="services__links">
+                    View on GitHub
+                  </a>
+                </button>
+              </div>
+          </div>
+          <div className='text-center shadow-lg p-10 my-10 basis-1/3 flex-1  dark:bg-gray-900'>
+              <h3 className='text-lg font-medium'>This website</h3>
+              <p className='text-gray-700 py-1 dark:text-white'>Tools used:</p>
+              <p className='text-gray-700 dark:text-white'>JS</p>
+              <p className='text-gray-700 dark:text-white'>React</p>
+              <p className='text-gray-700 dark:text-white'>Tailwind</p>
+              <div className= 'pt-5'>
+                <button class="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                  <a href="https://github.com/m-riosa/matias-wesbite" class="services__links">
+                    View on GitHub
+                  </a>
+                </button>
+              </div>
+          </div>
+        </div>
+       </section>
+      </main>
+    </div>
+  );
 }
